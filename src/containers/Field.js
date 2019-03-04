8eef51f388f1c298ef7a776963076fb5dd9aa48c
jaroslawwysocki <wysockijar@gmail.com>
2019-03-04 17:24:07 +0100

    Grid on the canvas initial logic

--- src/containers/Field.js
+++ src/containers/Field.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
 import gameFieldClasses from '../styles/gameField.module.css';
-// import useCanvasContext from '../custom-hooks/useCanvasContext';
 import { GeometricalParameters } from '../context';
 
 const field = () => {
@@ -9,18 +8,38 @@ const field = () => {
     width: 0,
     height: 0
   });
-  const gameFieldId = "gameField";
-  // const canvasContext = useCanvasContext(gameFieldId);
+  const canvasRef = React.createRef();
 
+  // TODO: passing [] as a second parameter to useEffect should generate behaviour
+  // as with componentDidMount. However, grid is not rendered then. Why?
   useEffect(() => {
     const width = geometricalParameters.numberOfColumns * geometricalParameters.fieldElementSizeInPx;
     const height = geometricalParameters.numberOfRows * geometricalParameters.fieldElementSizeInPx;
     setFieldDimensions({width, height});
-  }, []);
+    fillGrid();
+  });
+
+  const fillGrid = () => {
+    let canvas = canvasRef.current;
+    if (canvas) {
+      let canvasCtx = canvas.getContext('2d');
+      for (let i = 0; i < geometricalParameters.numberOfColumns; i++) {
+        for (let j = 0; j < geometricalParameters.numberOfRows; j++) {
+          const gridSize = geometricalParameters.fieldElementSizeInPx;
+          const x = i * gridSize;
+          const y = j * gridSize;
+          canvasCtx.fillStyle = 'border: 1px solid black';
+          canvasCtx.fillRect(x, y, gridSize, gridSize);
+          canvasCtx.strokeRect(x, y, gridSize, gridSize);
+          canvasCtx.clearRect(x, y, gridSize, gridSize);
+        }
+      }
+    }
+  }
 
   return (
     <canvas
-      id={gameFieldId}
+      ref={canvasRef}
       width={fieldDimensions.width}
       height={fieldDimensions.height}
       className={gameFieldClasses.GameField}
