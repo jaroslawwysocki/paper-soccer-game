be09682198fa2b45ba9a92704be41466902445f6
Jaroslaw Wysocki <wysockijar@gmail.com>
2019-03-04 23:08:43 +0100

    Coordinates calculation for move making

--- src/containers/Field.js
+++ src/containers/Field.js
@@ -11,22 +11,23 @@ const field = () => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
-    fillGrid();
-    console.log('useEffect called...');
-  }, []);
-
-  const fillGrid = () => {
     const canvas = canvasRef.current;
     if (canvas) {
       let canvasCtx = canvas.getContext('2d');
-      canvasCtx.fillStyle = 'border: 1px solid black';
-      for (let i = 0; i < geometricalParameters.numberOfRowsWithGates; i++)
-        for (let j = 0; j < geometricalParameters.numberOfColumns; j++)
-          if (isGateFragment(i, j)) {
-            drawFragment(j, i, canvasCtx);
-          } else if (!isGateRow(i)) {
-            drawFragment(j, i, canvasCtx);
-      }
+      fillGrid(canvasCtx);
+      canvas.addEventListener('click', handleMove);
+    }
+    return () => canvas.removeEventListener('click', handleMove);
+  }, []);
+
+  const fillGrid = canvasCtx => {
+    canvasCtx.strokeStyle = 'white';
+    for (let i = 0; i < geometricalParameters.numberOfRowsWithGates; i++)
+      for (let j = 0; j < geometricalParameters.numberOfColumns; j++)
+        if (isGateFragment(i, j)) {
+          drawFragment(j, i, canvasCtx);
+        } else if (!isGateRow(i)) {
+          drawFragment(j, i, canvasCtx);
     }
   }
 
@@ -44,9 +45,34 @@ const field = () => {
     const gridSize = geometricalParameters.fieldElementSizeInPx;
     const x = columnNumber * gridSize;
     const y = rowNumber * gridSize;
-    canvasCtx.fillRect(x, y, gridSize, gridSize);
     canvasCtx.strokeRect(x, y, gridSize, gridSize);
-    canvasCtx.clearRect(x, y, gridSize, gridSize);
+  }
+
+  const handleMove = event => {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const {x, y} = canvas.getBoundingClientRect();
+      const xCanvas = event.clientX - x;
+      const yCanvas = event.clientY - y;
+      const closestGridPoint = findClosestGridIntersectionPoint(xCanvas, yCanvas);
+      alert('x = ' + closestGridPoint.x + ', y = ' + closestGridPoint.y);
+    }
+  }
+
+  const findClosestGridIntersectionPoint = (xCanvas, yCanvas) => {
+    return {
+      x: findNearestGridCoordinate(xCanvas),
+      y: findNearestGridCoordinate(yCanvas)
+    };
+  }
+
+  const findNearestGridCoordinate = coordinate => {
+    const fieldElementSize = geometricalParameters.fieldElementSizeInPx;
+    const totalDivision = Math.floor(coordinate / fieldElementSize);
+    const smallerCoordinate = totalDivision * fieldElementSize;
+    const biggerCoordinate = (totalDivision + 1) * fieldElementSize;
+    return (coordinate - smallerCoordinate) < (biggerCoordinate - coordinate)
+      ? smallerCoordinate : biggerCoordinate;
   }
 
   return (
